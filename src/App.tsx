be5e0b3f04ba03fe27705a81def5fe4a5d193195
Breno Alves <breno550@hotmail.com>
2020-12-30 03:49:11 -0300

    toast message

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,14 @@ import GlobalStyle from './styles/global';
 import SignIn from './pages/Signin';
 import SignUp from './pages/SignUp';
 
-import { AuthProvider } from './hooks/AuthContext';
+import AppProvider from './hooks';
 
 const App: React.FC = () => {
   return (
     <>
-      <AuthProvider>
+      <AppProvider>
         <SignIn />
-      </AuthProvider>
+      </AppProvider>
 
       <GlobalStyle />
     </>
